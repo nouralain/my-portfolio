595b3507bb4cd8da68b9f7c560fd5fe1ead8f90b
nouralain <nouralain@users.noreply.github.com>
2026-06-19 14:58:23 +0300

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/nouralain"
                 target="_blank"
                 className="fork-btn-inner"
               >
